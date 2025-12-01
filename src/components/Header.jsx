2305d7fe677e48024fa0d8183116a6e58d9e49c0
HarshKotwal <harshkotwal1608@gmail.com>
2025-12-01 21:02:37 +0530

    added text and button to get meme

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header>
+    <header className="header">
       <img src="/smile.png" alt="meme icon" />
       <h1>heheMemes</h1>
     </header>
---
+++ src/components/Main.jsx
@@ -0,0 +1,17 @@
+export default function Main() {
+  return (
+    <main>
+      <div className="form">
+        <label>
+          Top Text
+          <input type="text" placeholder="Shut up" name="topText" />
+        </label>
+        <label>
+          Bottom Text
+          <input type="text" placeholder="Your mouth" name="topText" />
+        </label>
+        <button>Get a new Meme image</button>
+      </div>
+    </main>
+  );
+}
