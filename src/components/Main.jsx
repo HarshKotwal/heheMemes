2811017e925c7a01d79ca85ec4e0dd48dcb2625c
HarshKotwal <harshkotwal1608@gmail.com>
2025-12-03 12:36:27 +0530

    add meme input functionality using controlled components

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,16 +6,37 @@ export default function Main() {
     bottomText: "Your mouth",
     imgUrl: "/meme.png",
   });
+
+  function handleChange(event) {
+    const { value, name } = event.currentTarget;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      [name]: value,
+    }));
+  }
+
   return (
     <main>
       <div className="form">
         <label>
           Top Text
-          <input type="text" placeholder="Shut up" name="topText" />
+          <input
+            type="text"
+            placeholder="Shut up"
+            name="topText"
+            onChange={handleChange}
+            value={meme.topText}
+          />
         </label>
         <label>
           Bottom Text
-          <input type="text" placeholder="Your mouth" name="topText" />
+          <input
+            type="text"
+            placeholder="Your mouth"
+            name="bottomText"
+            onChange={handleChange}
+            value={meme.bottomText}
+          />
         </label>
         <button>Get a new Meme image</button>
       </div>
